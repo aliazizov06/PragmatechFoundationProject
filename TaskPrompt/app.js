79e401c4fcff918e0d681dcfdf1532ccd5b34926
Ali Azizov <aliazizov158@gmail.com>
2021-10-22 18:44:01 +0400

    task

---
+++ TaskPrompt/app.js
@@ -0,0 +1,11 @@
+let ad = prompt("Adinizi qeyd edin");
+let soyad = prompt("Soyadinizi qeyd edin");
+for(i=0; i<2; i++){
+    console.log(ad);
+}
+for(i=0; i<4; i++){
+    console.log(soyad);
+}
+console.log(ad + "|||" + soyad)
+console.log("Sizin adiniz : " +ad)
+console.log("Sizin soyadiniz : " +soyad)
