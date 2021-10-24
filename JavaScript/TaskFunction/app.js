c606cd204b42d974fa264b891a30ef41857b867f
Ali Azizov <aliazizov158@gmail.com>
2021-10-24 13:25:44 +0400

    function

---
+++ JavaScript/TaskFunction/app.js
@@ -0,0 +1,23 @@
+//Daxil edilən 5 parametri array formasında return edən funksiya yazın
+let a = 10
+    b = null
+    c = "Java"
+    d = "Pragmatech"
+    e = "Foundation45"
+
+function array(){
+    let x=[a,b,c,d,e]
+    console.log(x)
+    return x
+}
+array()
+
+//Parametr olaraq array qəbul edən və daxil edilən arrayın ilk elementini return edən funksiya yazın
+const langs = ["English", "Turkish", "Russian", "French"]
+
+function firstlang(){
+    let first = langs[0]
+    console.log(first)
+    return first
+}
+firstlang()
