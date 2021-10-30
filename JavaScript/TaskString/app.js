617243898a3a220e586716e306b820e3522287e6
Ali Azizov <aliazizov158@gmail.com>
2021-10-30 18:43:01 +0400

    string

---
+++ JavaScript/TaskString/app.js
@@ -0,0 +1,13 @@
+const str = 'Proqramlaşdırma nə qədər çox şey bildiyinizlə yox, bildiyinizlə ortaya çıxardığınız işlərlə maraqlanır'
+//str daxilində neçə xarakter olduğunu ekrana yazdırın
+let val = str.length
+
+//str daxilindəki sözləri ayrı bir massiv içərisində toplayın
+val = str.split(' ')
+
+//str daxilində neçə hərf olduğunu ekrana yazdırın
+val = str.replace(/ /g,'').replace(/,/g,'').length
+
+//
+val = str.split("-1")
+console.log(val)
