ee58e76348284a58bc9c21f23c1797bfb051e8ed
Ali Azizov <aliazizov158@gmail.com>
2021-11-03 18:04:37 +0400

    DOM

---
+++ JavaScript/bgChanger/app.js
@@ -0,0 +1,5 @@
+//Background Changer - buttona clickləyəndə arxa fonun rəngi dəyişsin və həmin rəngin adı buttonun üstündə yazılsın
+changeColor = document.querySelector("body")
+function makeRed(){
+    changeColor.style.backgroundColor = "red"
+}
