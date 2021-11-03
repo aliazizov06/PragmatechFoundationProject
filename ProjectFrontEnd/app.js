486a91b2726688decd0bf9790fa15a9ca67d60a8
Ali Azizov <aliazizov158@gmail.com>
2021-11-03 13:58:05 +0400

    MyPortfolio

---
+++ ProjectFrontEnd/app.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector("#sidebar-menu")
+let menuIcon = document.querySelector(".menu-icon")
+//menu.style.display="none"
+function closeMenu(){
+    menu.style.display="none"
+}
