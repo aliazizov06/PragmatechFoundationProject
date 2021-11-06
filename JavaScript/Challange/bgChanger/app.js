d2c12a831101f9446f549c03b3a096b6e3619d54
Ali Azizov <aliazizov158@gmail.com>
2021-11-06 20:20:37 +0400

    challenge

--- JavaScript/Challange/bgChanger/app.js
+++ JavaScript/Challange/bgChanger/app.js
@@ -1,5 +1,11 @@
 //Background Changer - buttona clickləyəndə arxa fonun rəngi dəyişsin və həmin rəngin adı buttonun üstündə yazılsın
-changeColor = document.querySelector("body")
-function makeRed(){
-    changeColor.style.backgroundColor = "red"
+let bodyColor = document.querySelector("body")
+let colorName = document.querySelector("span")
+function changeColor(){
+    bodyColor.style.backgroundColor = 
+    'rgb('+Math.round(Math.random()*255)+','
+    + Math.round(Math.random()*255)+','
+    + Math.round(Math.random()*255)+')'
+    colorName.innerText =   `Background Color: ${bodyColor.style.backgroundColor}` 
+
 }
