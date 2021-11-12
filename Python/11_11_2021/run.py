c156e0b1fd88bc47ecadc171f5a36506cb6326d1
Ali Azizov <aliazizov158@gmail.com>
2021-11-12 17:48:00 +0400

    11_11_2021

---
+++ Python/11_11_2021/run.py
@@ -0,0 +1,36 @@
+telebeler=[
+    {
+    'ad':'Samir',
+    'soyad':'Hemidov',
+    'yas':30
+    },
+    {
+    'ad':'Memmed',
+    'soyad':'Salamov',
+    'yas':20
+    },
+    {
+    'ad':'Aliye',
+    'soyad':'Qurbanova',
+    'yas':16
+    },
+    {
+    'ad':'Sahil',
+    'soyad':'Qeniyev',
+    'yas':45
+    },
+    {
+    'ad':'Ehmed',
+    'soyad':'Qeniyev',
+    'yas':50
+    }
+]
+#Butun adlari ekrana cap edin
+for i in telebeler:
+    print(i['ad'])
+#Istifadecilerin yaslarinin cemini tapin
+for i in telebeler:
+    print(i['yas'])
+
+
+#adi Ehmed olan istifadecinin butun melumatlarini ekrana cap edin
