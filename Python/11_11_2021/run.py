c00c33e67395c7a2b31cbd8e7f871e83d678c181
Ali Azizov <aliazizov158@gmail.com>
2021-11-17 17:18:01 +0400

    task

--- Python/11_11_2021/run.py
+++ Python/11_11_2021/run.py
@@ -37,3 +37,24 @@ for j in telebeler:
 print(sum)
 
 #adi Ehmed olan istifadecinin butun melumatlarini ekrana cap edin
+for telebe in telebeler:
+    if telebe['ad']=='Ehmed':
+        print(telebe)
+
+#findUser(userName) adli metod yaradin. daxil edilen ada əsasən o istifadecinin butun melumatlarini ekrana cap etsin
+def findUser(userName):
+    for user in telebeler:
+        if userName==user['ad']:
+            print(f"User information: {user}")
+    return user
+findUser('Memmed')
+    
+#İstifadecilerin yas ortalamasini tapin. Yasi ortalama yasdan boyuk olan istifadecilerin melumatlarini ekrana cap edin
+sumOfAges = 0
+for t in telebeler:
+    sumOfAges+=t['yas']
+sumOfAges/=len(telebeler)
+if t['yas']>sumOfAges:
+    print(f"Yasi ortalamadan boyuk olanlar: {t}")
+
+#soyadlari eyni olan istifadecilerden yasi en boyuk olanin adini ekrana cap edin
