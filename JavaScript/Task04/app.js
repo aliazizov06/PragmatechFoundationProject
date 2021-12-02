b34b33e01dd00dc89d33a78c41742af6b777a53a
Ali Azizov <aliazizov158@gmail.com>
2021-12-02 15:02:11 +0400

    js

--- JavaScript/Task04/app.js
+++
@@ -1,4 +0,0 @@
-let person = prompt("Adinizi qeyd edin");
-for(let i=1; i<=10; i++){
-    document.write(person + "<br>");
-}
--- JavaScript/TaskPrompt/app.js
+++ JavaScript/TaskPrompt/app.js
@@ -9,3 +9,26 @@ for(i=0; i<4; i++){
 console.log(ad + "|||" + soyad)
 console.log("Sizin adiniz : " +ad)
 console.log("Sizin soyadiniz : " +soyad)
+
+//////////////////////////////////////////////////
+
+let age = prompt("Yasinizi qeyd edin");
+ 
+if(age<30 && age>0){
+  let a=age*age;
+  document.write(a);
+}
+if(age>=30 && age<=40){
+    let b=age%10;
+    document.write(b);
+}
+if(age<0 || age>100){
+    alert("Duzgun melumat daxil etmemisiniz");
+}
+
+/////////////////////////////////////////////////////
+
+let person = prompt("Adinizi qeyd edin");
+for(let i=1; i<=10; i++){
+    document.write(person + "<br>");
+}
